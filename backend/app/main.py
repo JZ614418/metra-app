3219b1973dd18bc59149ac2299496840c71655d3
JZ <jzhe614@gmail.com>
2025-06-29 04:11:52 -0700

    Refactor CORS settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,13 +10,14 @@ app = FastAPI(
 )
 
 # Set all CORS enabled origins
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["https://metratraining.com", "http://localhost:3000", "http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
